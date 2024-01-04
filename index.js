6715574993072fe6b3749aa62f4d9a3db1a4453c
wpmohammad1 <alimohammadjhd@gmail.com>
2024-01-05 01:06:54 +0600

    team member and  get user also appoinment

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
- 
+
+//middle ware  
 app.use(cors());
 app.use(express.json())
-
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e60xkn0.mongodb.net/?retryWrites=true&w=majority`;
@@ -26,22 +25,51 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const userCollection = client.db('doctorAppoinment').collection('users')
     const teamMemberCollection = client.db('doctorAppoinment').collection('team')
-
+    const appoinmentBookingCollection = client.db('doctorAppoinment').collection('appoinment')
     
+
+    // get all user data
+    app.post('/users', async(req, res) =>{
+      const user = req.body;
+      console.log(user)
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+  })
+  // get all appoinment data
+    app.get('/users', async(req, res) =>{
+      const user =await userCollection.find().toArray();
+      res.send(user)
+  })
+
     // get all team member data
     app.get('/team', async(req, res) =>{
         const team =await teamMemberCollection.find().toArray();
         res.send(team)
-        console.log(team)
     })
+    // get all single team details data
     app.get('/team/:id', async(req, res) =>{
-      const memberId = req.params.id;
-      const teamMember = await teamMemberCollection.findOne({ _id: ObjectId(memberId) });
-      res.send(teamMember)
-      console.log(teamMember)
-
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await teamMemberCollection.findOne(query)
+      res.send(result)
     })
+
+    // get all appoinment data
+    app.post('/appoinment', async(req, res) =>{
+      const appoinment = req.body;
+      console.log(appoinment)
+      const result = await appoinmentBookingCollection.insertOne(appoinment)
+      res.send(result)
+  })
+  // get all appoinment data
+    app.get('/appoinment', async(req, res) =>{
+      const appoinment =await appoinmentBookingCollection.find().toArray();
+      res.send(appoinment)
+  })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
