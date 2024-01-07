b9629da15b36a6a5610736d5e44cc0860bbb52d1
wpmohammad1 <alimohammadjhd@gmail.com>
2024-01-07 23:59:36 +0600

    get a single  blog data

--- index.js
+++ index.js
@@ -106,7 +106,13 @@ async function run() {
     const result = await blogCollection.find().toArray();
     res.send(result)
   })
-
+  // get a single blog data by id
+  app.get('/blogs/:id', async(req, res) =>{
+    const id = req.params.id;
+    const query = {_id : new ObjectId(id)}
+    const result = await blogCollection.findOne(query)
+    res.send(result)
+  })
 
 
     // Send a ping to confirm a successful connection
