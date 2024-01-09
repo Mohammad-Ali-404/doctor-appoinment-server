2c412143ec8394b63c9df0f33c00545da13d478f
wpmohammad1 <alimohammadjhd@gmail.com>
2024-01-10 01:52:45 +0600

    verify jwt token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
@@ -9,6 +10,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+const veifyJWT = (req, res, next) =>{
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({error: true, message:'unauthorizes access'})
+  }
+  // bearer token
+
+  const token = authorization.split(' ')[1]
+
+  // verify a token symmetric
+
+  jwt.verify(token, process.env.ACCESS_TOEKN_SECRET, function(err, decoded) {
+    if (err) {
+      return res.status(401).send({error: true, message:'unauthorizes access'})
+    }
+    req.decoded = decoded;
+    next()
+    console.log(decoded.foo) // bar
+  });
+ }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e60xkn0.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -34,10 +55,15 @@ async function run() {
     const blogCollection = client.db('doctorAppoinment').collection('blogs')
 
 
+    // get a jwt token
+    app.post('/jwt', (req, res) =>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOEKN_SECRET, { expiresIn: '1h' })
+      res.send({token})
+    })
     // get all user data by id
     app.post('/users', async(req, res) =>{
       const user = req.body;
-      console.log(user)
       const query = {email: user.email}
       const exstingUser = await userCollection.findOne(query)
       if (exstingUser) {
